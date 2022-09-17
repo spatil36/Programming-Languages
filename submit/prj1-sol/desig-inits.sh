1f84507368af55c443a61d9814afa16046057d88
shubham <spatil36.spatil36@gmail.com>
2022-09-16 22:13:54 -0400

    started prj1

---
+++ submit/prj1-sol/desig-inits.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+#sets dir to directory containing this script
+dir=`dirname $0`
+
+#use $dir/ as prefix to run any programs in this dir
+#so that this script can be run from any directory.
+
+
