6d5d28c8b2895afb11d3ee0b53270e8fa4d6aa39
shubham <spatil36.spatil36@gmail.com>
2022-09-18 19:32:06 -0400

    prj1 completition

--- submit/prj1-sol/desig-inits.sh
+++ submit/prj1-sol/desig-inits.sh
@@ -5,5 +5,7 @@ dir=`dirname $0`
 
 #use $dir/ as prefix to run any programs in this dir
 #so that this script can be run from any directory.
+set classpath="$dir/"
+java Prj1Parser
 
 
--- submit/prj1-sol/make.sh
+++ submit/prj1-sol/make.sh
@@ -5,7 +5,12 @@ dir=`dirname $0`
 
 #use $dir to access programs in this directory
 #so that this script can be run from any directory.
+if (javac "$dir/Prj1Parser.java") ; then
+	
+	echo "build done"
+else
 
-echo "no build done"
+	echo "no build done"
+fi
 
 
